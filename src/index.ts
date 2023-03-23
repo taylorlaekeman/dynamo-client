2ac497edc30b97a865950d0555f0224b505f111b
taylor <taylor.laekeman@gmail.com>
2023-03-22 20:59:23 -0400

    add exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import client from './client';
+
+export { default as ExistingTableError } from './errors/existingTableError';
+export { default as MissingKeyError } from './errors/missingKeyError';
+export { default as MissingLocationError } from './errors/missingLocationError';
+export { default as MissingTableError } from './errors/missingTableError';
+export { default as StorageError } from './errors/storageError';
 export { default as DynamoEngine } from './dynamoEngine';
 export { default as FileEngine } from './fileEngine';
 export { default as MemoryEngine } from './memoryEngine';
+
+export default client;
